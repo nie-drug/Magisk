b1ecb871e4ac980a7dac012c5e52de920c80e4be
Neil A <76754251+nie-drug@users.noreply.github.com>
2021-08-25 02:09:01 +0400

    Update SettingsFragment.kt

--- app/src/main/java/com/topjohnwu/magisk/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/settings/SettingsFragment.kt
@@ -29,7 +29,7 @@ class SettingsFragment : BaseUIFragment<SettingsViewModel, FragmentSettingsMd2Bi
             navigateUp()
         }
         binding.toolbar.setNavigationIcon(R.drawable.quantum_gm_ic_arrow_back_vd_theme_24)
-        binding.toolbar.setTitle(R.string.section_settings)
+        binding.toolbar.setTitle(R.string.settings)
         binding.settingsList.setOnViewReadyListener {
             binding.settingsList.scrollToPosition(0)
         }
