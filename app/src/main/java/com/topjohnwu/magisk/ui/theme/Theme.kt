d774134753440c079feddab8caa0c5eca33698c8
Neil A <76754251+nie-drug@users.noreply.github.com>
2021-08-24 17:53:29 +0400

    Update Theme.kt

--- app/src/main/java/com/topjohnwu/magisk/ui/theme/Theme.kt
+++ app/src/main/java/com/topjohnwu/magisk/ui/theme/Theme.kt
@@ -40,10 +40,6 @@ enum class Theme(
         themeName = "Fraxure (Legacy)",
         themeRes = R.style.ThemeFoundationMD2_Fraxure
     ),
-    Monet(
-        themeName = "Monet",
-        themeRes = R.style.ThemeFoundationMD3_Monet
-    );
     
     val isSelected get() = Config.themeOrdinal == ordinal
 
